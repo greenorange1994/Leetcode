61557f53c5921002b8ae36dbf3df7df938ca643f
greenorange1994 <yz3045@columbia.edu>
2017-09-08 22:11:07 -0400

    create2

--- Array/Find_All_Duplicates_in_an_Array.java
+++ Array/Find_All_Duplicates_in_an_Array.java
@@ -1,1 +1,13 @@
+import java.lang.*;
 
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]) - 1; 
+            if(nums[index] > 0) nums[index] = -nums[index];
+            else if(nums[index] < 0) res.add(index + 1);
+        }
+        return res;
+    }
+}
